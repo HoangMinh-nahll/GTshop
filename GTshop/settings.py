3fda694ccaea85e121aec6007dbd65ea7ac52014
datld47 <datldsave@gmail.com>
2026-04-08 13:36:11 +0700

    Da Add them 7 tin tuc

--- GTshop/settings.py
+++ GTshop/settings.py
@@ -107,3 +107,5 @@ LOGIN_REDIRECT_URL = 'store:home'
 LOGOUT_REDIRECT_URL = 'store:home'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
